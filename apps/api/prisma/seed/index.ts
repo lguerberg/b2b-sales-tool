40fd0aa084867a4c3f65028eaef1f237c2c5846c
Leo Guerberg <leonardo.guerberg@zerf.tech>
2024-06-04 14:58:47 -0300

    Add users seeder

--- apps/api/prisma/seed/index.ts
+++ apps/api/prisma/seed/index.ts
@@ -1,44 +1,94 @@
+import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
-import {faker} from "@faker-js/faker"
-import { createCompany, createLead, createLocation } from './utils'
+
+import { UsersToSeed, createCompany, createLead, createLocation } from './utils'
 
 const prisma = new PrismaClient()
 
 async function main() {
-    console.log('Start seeding...')
-    // Seed locations
-    const locations = await prisma.location.createManyAndReturn({
-      data: faker.helpers.multiple(createLocation, {
-        count: 1000
-      }),
-      select: {
-        id: true
-      }
-    })
+  console.log('Start seeding...')
 
-    // Seed companies
-    const companies = await prisma.company.createManyAndReturn({
-      data: faker.helpers.multiple(() => createCompany(faker.helpers.arrayElement(locations).id), {
-        count: 10000
-      }),
-      select: {
-        id: true
-      }
-    })
+  // Seed company for demoing
+  const demoCompany = await prisma.company.upsert({
+    where: {
+      name: 'Nuvia',
+    },
+    update: {},
+    create: {
+      name: 'Nuvia',
+      primaryColor: faker.color.rgb(),
+      secondaryColor: faker.color.rgb(),
+      size: 'FIVE_HUNDRED_ONE_TO_ONE_THOUSAND',
+      industry: 'Software',
+      type: 'Private',
+      hqLocation: {
+        create: {
+          city: 'Buenos Aires',
+          country: 'AR',
+          continent: 'South America',
+        },
+      },
+      onboard: {
+        create: {
+          conversionRate: 1,
+          mediumTicketPrice: 2670,
+          salesSpeechContext:
+            'You are a the BDR of Nuvia.co, a company that provides a SaaS product for sales teams. Your SaaS includes prospecting, enrichment, outreach, and CRM features (most of the pains that address fintechs)',
+          calendlyUrl: 'https://calendly.com/leo-guerberg/sales-demo',
+        },
+      },
+    },
+    select: {
+      id: true,
+    },
+  })
 
-    // Seed leads
-    await prisma.lead.createMany({
-      data: faker.helpers.multiple(() => createLead(faker.helpers.arrayElement(companies).id), {
-        count: 10000,
-      })
-    })
+  // Seed locations
+  const locations = await prisma.location.createManyAndReturn({
+    data: faker.helpers.multiple(createLocation, {
+      count: 1000,
+    }),
+    select: {
+      id: true,
+    },
+  })
+
+  // Seed users
+  await Promise.all(
+    (await UsersToSeed(demoCompany.id)).map(async user =>
+      prisma.user.upsert({
+        where: {
+          email: user.email,
+        },
+        update: {},
+        create: user,
+      }),
+    ),
+  )
+
+  // Seed companies
+  const companies = await prisma.company.createManyAndReturn({
+    data: faker.helpers.multiple(() => createCompany(faker.helpers.arrayElement(locations).id), {
+      count: 10000,
+    }),
+    select: {
+      id: true,
+    },
+  })
+
+  // Seed leads
+  await prisma.lead.createMany({
+    data: faker.helpers.multiple(() => createLead(faker.helpers.arrayElement(companies).id), {
+      count: 10000,
+    }),
+  })
 }
 main()
   .then(async () => {
     console.log('Seed completed')
     await prisma.$disconnect()
   })
-  .catch(async (e) => {
+  .catch(async e => {
     console.error(e)
     await prisma.$disconnect()
     process.exit(1)
--- apps/api/prisma/seed/utils.ts
+++ apps/api/prisma/seed/utils.ts
@@ -1,44 +1,73 @@
-import {faker} from '@faker-js/faker'
+import { faker } from '@faker-js/faker'
 import { CompanySize, Seniority } from '@prisma/client'
 
+import { hashPassword } from '../../src/infrastructure/utils/password.utils'
+
 export const createLocation = () => ({
-    city: faker.location.city(),
-    country: faker.location.countryCode(),
-    continent: faker.helpers.arrayElement(['Africa', 'Antarctica', 'Asia', 'Europe', 'North America', 'Oceania', 'South America']),
+  city: faker.location.city(),
+  country: faker.location.countryCode(),
+  continent: faker.helpers.arrayElement([
+    'Africa',
+    'Antarctica',
+    'Asia',
+    'Europe',
+    'North America',
+    'Oceania',
+    'South America',
+  ]),
 })
 
 export const createCompany = (hqLocationId: string) => ({
-    name: faker.company.name(),
-    primaryColor: faker.color.rgb(),
-    secondaryColor: faker.color.rgb(),
-    size: faker.helpers.enumValue(CompanySize),
-    industry: faker.commerce.department(),
-    type: faker.helpers.arrayElement([
-        'Public',
-        'Private',
-        'Non-profit',
-        'Government-owned',
-        'Multinational',
-        'Startup',
-        'Family-owned',
-        'Joint venture',
-        'Subsidiary',
-        'Cooperative'
-    ]),
-    hqLocationId
-  })
+  name: faker.company.name(),
+  primaryColor: faker.color.rgb(),
+  secondaryColor: faker.color.rgb(),
+  size: faker.helpers.enumValue(CompanySize),
+  industry: faker.commerce.department(),
+  type: faker.helpers.arrayElement([
+    'Public',
+    'Private',
+    'Non-profit',
+    'Government-owned',
+    'Multinational',
+    'Startup',
+    'Family-owned',
+    'Joint venture',
+    'Subsidiary',
+    'Cooperative',
+  ]),
+  hqLocationId,
+})
 
-  export const createLead = (currentCompanyId: string) => ({
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    email: faker.internet.email(),
-    phone: faker.phone.number(),
-    enrichedSummary: faker.lorem.paragraph(),
-    currentCompanyId,
-    pastCompanyId: currentCompanyId,
-      seniority: faker.helpers.enumValue(Seniority),
-      yearsInCurrentPosition: faker.datatype.number({max: 20}),
-      jobTitle: faker.name.jobTitle(),
-      jobDescription: faker.name.jobDescriptor(),
-      isDecisionMaker: faker.datatype.boolean()
-  })
+export const createLead = (currentCompanyId: string) => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.email(),
+  phone: faker.phone.number(),
+  enrichedSummary: faker.lorem.paragraph(),
+  currentCompanyId,
+  pastCompanyId: currentCompanyId,
+  seniority: faker.helpers.enumValue(Seniority),
+  yearsInCurrentPosition: faker.datatype.number({ max: 20 }),
+  jobTitle: faker.name.jobTitle(),
+  jobDescription: faker.name.jobDescriptor(),
+  isDecisionMaker: faker.datatype.boolean(),
+})
+
+export const UsersToSeed = async (companyId: string) => [
+  {
+    email: 'leo.guerberg@gmail.com',
+    firstName: 'Leo',
+    lastName: 'Guerberg',
+    password: await hashPassword('leo.guerberg'),
+    avatarUrl: faker.image.avatar(),
+    companyId,
+  },
+  {
+    email: 'gabriel.benmergui@gmail.com',
+    firstName: 'Gabriel',
+    lastName: 'Benmergui',
+    password: await hashPassword('gabriel.benmergui'),
+    avatarUrl: faker.image.avatar(),
+    companyId,
+  },
+]
