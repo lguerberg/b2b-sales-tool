b896d6b622e95fc6d9421b66efd9171e3a02bb36
Leo Guerberg <leonardo.guerberg@zerf.tech>
2024-06-04 15:40:30 -0300

    Fix user middleware return

--- apps/api/src/infrastructure/decorators/logged-user.decorator.ts
+++ apps/api/src/infrastructure/decorators/logged-user.decorator.ts
@@ -1,6 +1,8 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common'
 
+import { User } from '@/domain/user'
+
 export const LoggedUser = createParamDecorator((_, context: ExecutionContext) => {
   const request = context.switchToHttp().getRequest()
-  return request.loggedUser?.id
+  return request.loggedUser as User
 })
