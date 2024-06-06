31f5b46d058b63ecacf04beafe99dfb55fc99c09
Leo Guerberg <leonardo.guerberg@zerf.tech>
2024-06-06 12:01:03 -0300

    Adjust homepage alignment

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import LineChart from '@app/components/charts/LineChart'
-import PieChart from '@app/components/charts/PieChart'
 import MetricsCard from '@app/components/dashboard/MetricsCard'
+import SectionTitle from '@app/components/titles/SectionTitle'
 
 export default function Dashboard() {
   const metrics = {
@@ -12,9 +11,9 @@ export default function Dashboard() {
   }
 
   return (
-    <div className="max-w-7xl mx-auto flex flex-col gap-10">
+    <div className="flex flex-col gap-10">
       <div className="flex flex-col gap-5">
-        <p className="text-xl lg:text-2xl font-extrabold text-center">Welcome Leo! Here is a resume of your activity</p>
+        <SectionTitle>Welcome Leo! Here is a resume of your activity</SectionTitle>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-evenly gap-4">
           <MetricsCard title="Campaigns" Icon={() => <div>Icon</div>}>
             <div className="min-w-40">{metrics.campaigns}</div>
@@ -32,15 +31,7 @@ export default function Dashboard() {
       </div>
 
       <div className="flex flex-col">
-        <p className="text-xl lg:text-2xl font-extrabold text-center mb-5">
-          Check this potential data about your potential leads
-        </p>
-        <div className="grid grid-rows-2 gap-5">
-          <div className="grid grid-cols-2 gap-5">
-            <PieChart />
-            <PieChart />
-          </div>
-        </div>
+        <SectionTitle>Check this potential data about your potential leads</SectionTitle>
       </div>
     </div>
   )
