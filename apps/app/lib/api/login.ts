87b7ea8e8e7d66153c20b3a4869caed9d89b44ec
Leo Guerberg <leonardo.guerberg@zerf.tech>
2024-06-09 16:29:47 -0300

    Fix localhost ssr fetch url

--- apps/app/lib/api/login.ts
+++ apps/app/lib/api/login.ts
@@ -1,7 +1,7 @@
 import { LoginSchema } from '../schemas/login'
 
 export const login = (values: LoginSchema) =>
-  fetch('/api/login', {
+  fetch(`${process.env.NEXT_API_URL}/api/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -10,6 +10,6 @@ export const login = (values: LoginSchema) =>
   })
 
 export const logout = () =>
-  fetch('/api/logout', {
+  fetch(`${process.env.NEXT_API_URL}/api/logout`, {
     method: 'POST',
   })
--- apps/app/lib/hooks/mutations/useLogin.ts
+++ apps/app/lib/hooks/mutations/useLogin.ts
@@ -2,6 +2,7 @@
 
 import { useToast } from '@app/components/ui/use-toast'
 import { ApiError } from '@app/lib/api'
+import { login } from '@app/lib/api/login'
 import { ROUTES } from '@app/lib/constants'
 import { LoginSchema } from '@app/lib/schemas/login'
 import { useMutation } from '@tanstack/react-query'
@@ -14,13 +15,7 @@ export default function useLogin() {
   return useMutation({
     mutationKey: ['Login'],
     mutationFn: async (values: LoginSchema) => {
-      const res = await fetch('/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(values),
-      })
+      const res = await login(values)
 
       if (!res.ok) {
         const error: ApiError = await res.json()
